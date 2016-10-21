adcb70fb7d658a9e62a7f64895a20e0820bdac25
Muhammad Surya Ihsanuddin <surya.kejawen@gmail.com>
2016-10-21 20:59:13 +0700

    update

---
+++ src/AppBundle/Form/GroupType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Group;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GroupType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('code', TextType::class)
+            ->add('name', TextType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Group::class,
+        ));
+    }
+}
